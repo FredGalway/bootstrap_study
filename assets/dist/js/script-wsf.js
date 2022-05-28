2b363ecec73ac1e6a4464a69196804693c80f385
Fred M <fmoitry@gmail.com>
2022-05-28 02:16:51 +0200

    Navigaion terminée pour desktop / Reste à faire adaptation mobile

---
+++ assets/dist/js/script-wsf.js
@@ -0,0 +1,19 @@
+//alert("Hello! I am an alert box!!");
+
+//Fix navigation menu active links
+
+/*
+var myCollapseEl = document.getElementById('myCollapse')
+
+myCollapseEl.addEventListener('shown.bs.collapse', function(event) {
+    // Action to execute once the collapsible area is expanded
+    document.getElementById('navbarCollapse').style.color = "green";
+})
+*/
+function init() {
+    document.getElementById("nav-socialnetwork-icon1").src = '../css/img/nav-facebook-icon-blue.svg';
+
+}
+
+
+window.onload = init();
